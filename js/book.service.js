be3a875e3e8a947b222036b510c79502878fdb0a
Marchello92 <mark.davidovich@gmail.com>
2024-02-16 11:13:54 +0200

    Add a base to work with

--- js/book.service.js
+++ js/book.service.js
@@ -1,1 +1,29 @@
 'use strict'
+
+var gBooks = [
+    {
+        id: 1,
+        title: 'book1',
+        price: 120,
+        imgUrl: 'nothingATM1.jpg'
+    },
+
+    {
+        id: 2,
+        title: 'book2',
+        price: 200,
+        imgUrl: 'nothingATM2.jpg'
+    },
+
+    {
+        id: 3,
+        title: 'book3',
+        price: 400,
+        imgUrl: 'nothingATM3.jpg'
+    },
+
+]
+
+function getBooks() {
+    if (gBooks) return gBooks
+}
