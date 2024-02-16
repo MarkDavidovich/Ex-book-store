1c568f3060b66dbca2404334a51d11f8e8c6955d
Marchello92 <mark.davidovich@gmail.com>
2024-02-16 11:49:05 +0200

    Add dynamic book rendering and a few style changes

--- js/book.controller.js
+++ js/book.controller.js
@@ -6,4 +6,10 @@ function onInit() {
 
 function renderBooks() {
 
+    const strHTMLs = gBooks.map(book => `<tr><td>${book.title}</td><td>${book.price}</td><td>
+    <button class="read-btn">read</button><button class="update-btn">update</button><button
+            class="delete-btn">delete</button></td></tr>`)
+
+    const elBookTable = document.querySelector('.table-container')
+    elBookTable.innerHTML += strHTMLs.join('')
 }
